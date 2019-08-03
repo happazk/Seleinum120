6dfda5d62624928b5b3b3eb5e45a9cc44353cd1d
happazk <578863544@qq.com>
2019-08-03 13:02:38 +0800

    增加注册脚本: 1、弹出框处理以及调用javascript脚本处理 2、增加验证码处理，通过session和mysql完成

--- thinksns/thinksns_common/__init__.py
+++ thinksns/thinksns_common/__init__.py
@@ -1,1 +1,2 @@
 from .common import login_thinksns
+from .mysql_operation import get_verfied_code
---
+++ thinksns/thinksns_common/mysql_operation.py
@@ -0,0 +1,29 @@
+import pymysql
+
+
+def get_verfied_code(session):
+    """
+    function；获取验证码
+    :param session:
+    :return:
+    """
+    select_sql = "select code from ts_verifycode where session = '%s' ORDER BY id DESC limit 0,1" % session
+    ver_code = excute_mysql(select_sql)[0][0]
+    return ver_code
+
+
+def excute_mysql(select_sql):
+    # host='localhost',
+    # port=3310,
+    # user='woider',
+    # passwd='3243',
+    # db='python',
+    # charset='utf8'
+    # db = pymysql.connect(host="192.168.102.151",user= "root", passwd="123456",db= "thinksns")
+    db =pymysql.connect(host="192.168.102.151",user= "root", db= "thinksns")
+    # 创建游标
+    cursor = db.cursor()
+    # 执行sql语句
+    cursor.execute(select_sql)
+    data = cursor.fetchall()
+    return data
